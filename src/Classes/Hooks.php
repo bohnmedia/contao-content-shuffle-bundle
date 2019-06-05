31ac25b68089f037125994ce57a334ffaeedd48a
Dennis Bohn <dennis.bohn@bohn.media>
2019-06-05 11:43:22 +0000

    Added option to set max length of content elements

--- src/Classes/Hooks.php
+++ src/Classes/Hooks.php
@@ -10,6 +10,8 @@ class Hooks {
 		$elements = array();
 		$elementsShuffled = array();
 		$i = 0;
+		$maxlength = 0;
+		
 		
 		$objCte = \ContentModel::findPublishedByPidAndTable($objModule->id, 'tl_article');
 		
@@ -23,15 +25,23 @@ class Hooks {
 			{
 				if ($insideWrapper) return false;
 				$insideWrapper = true;
+				$maxlength = (int)$objRow->numberOfContentElements;
 				array_push($elements, $objModule->Template->elements[$i]);
 			}
 			else if ($objRow->type === "shuffle_stop")
 			{
+				// SHUFFLE ELEMENTS
 				shuffle($elementsShuffled);
+				
+				// SLICE ELEMENTS
+				if ($maxlength) $elementsShuffled = array_slice($elementsShuffled, 0, $maxlength);
+					
+				// MERGE SHUFFLED ELEMENTS
 				$elements = array_merge($elements, $elementsShuffled);
+				
+				array_push($elements, $objModule->Template->elements[$i]);
 				$elementsShuffled = array();
 				$insideWrapper = false;
-				array_push($elements, $objModule->Template->elements[$i]);
 			}
 			else if ($insideWrapper)
 			{
--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,4 +1,12 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['shuffle_start'] = '{type_legend},type';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['shuffle_start'] = '{type_legend},type,numberOfContentElements';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['shuffle_stop'] = '{type_legend},type';
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['numberOfContentElements'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['numberOfContentElements'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('rgxp'=>'natural', 'tl_class'=>'w50'),
+	'sql'                     => "smallint(5) unsigned NOT NULL default 0"
+);
